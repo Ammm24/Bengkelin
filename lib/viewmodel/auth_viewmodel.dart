923cf5780301806b9bde58552463405976c90808
reonaldi152 <reonaldi@team.superfk.co>
2025-07-12 09:23:20 +0700

    add login & register viewmodel

---
+++ lib/viewmodel/auth_viewmodel.dart
@@ -0,0 +1,35 @@
+
+import '../config/endpoint.dart';
+import '../config/model/resp.dart';
+import '../config/network.dart';
+
+class AuthViewmodel {
+  Future<Resp> login({email, password}) async {
+
+    Map<String, dynamic> formData = {
+      "email": email,
+      "password": password,
+    };
+
+    var resp = await Network.postApi(Endpoint.authLoginUrl, formData);
+    var data = Resp.fromJson(resp);
+    return data;
+  }
+
+  Future<Resp> register({name, email, phone, password, confirmPassword, kecamatanId, kelurahanId}) async {
+
+    Map<String, dynamic> formData = {
+      "name": name,
+      "email": email,
+      "phone_number": phone,
+      "kecamatan_id": kecamatanId,
+      "kelurahan_id": kelurahanId,
+      "password": password,
+      "password_confirmation": confirmPassword,
+    };
+
+    var resp = await Network.postApi(Endpoint.authRegisterUrl, formData);
+    var data = Resp.fromJson(resp);
+    return data;
+  }
+}
